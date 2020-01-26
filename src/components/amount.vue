12cf4553bcd8c59cb86ee84541355d44a2605ff2
mhxyzxx <mhxyzxx@163.com>
2020-01-26 09:53:33 +0800

    使用getters包装数据并总结vuex的使用方式

--- src/components/amount.vue
+++ src/components/amount.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h3>当前数量为：</h3>  
+    <!-- <h3>当前数量为：{{ $store.state.count }}</h3>   -->
+    <!-- 动态的去加一个字符串,使用第一种方式： -->
+    <!-- <h3>{{ '当前数量为：' + $store.state.count }}</h3>   -->
+    <!-- 使用第二种方式： -->
+    <h3>{{ $store.getters.optCount }}</h3>
   </div>
 </template>
 
@@ -8,5 +12,4 @@
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -26,10 +26,22 @@ var store = new Vuex.Store({
       console.log(obj)
       state.count -= (obj.c + obj.d)
     }
+  },
+  getters: {
+    // 注意：这里的 getters， 只负责 对外提供数据，不负责 修改数据，如果想要修改 state 中的数据，请 去找 mutations
+    optCount: function (state) {
+      return '当前最新的count值是：' + state.count
+    }
+    // 经过咱们回顾对比，发现 getters 中的方法， 和组件中的过滤器比较类似，因为 过滤器和 getters 都没有修改原数据， 都是把原数据做了一层包装，提供给了 调用者；
+    // 其次， getters 也和 computed 比较像， 只要 state 中的数据发生变化了，那么，如果 getters 正好也引用了这个数据，那么 就会立即触发 getters 的重新求值；
   }
     
 })
-
+// 总结：
+// 1. state中的数据，不能直接修改，如果想要修改，必须通过 mutations
+// 2. 如果组件想要直接 从 state 上获取数据： 需要 this.$store.state.***
+// 3. 如果 组件，想要修改数据，必须使用 mutations 提供的方法，需要通过 this.$store.commit('方法的名称'， 唯一的一个参数)
+// 4. 如果 store 中 state 上的数据， 在对外提供的时候，需要做一层包装，那么 ，推荐使用 getters, 如果需要使用 getters ,则用 this.$store.getters.***
 const vm = new Vue({
   el: '#app',
   render: c => c(App),
